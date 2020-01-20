510463d82e27a1f8630663a2481e3ceb08ca162c
Felix Andreas <felix.andreas95@googlemail.com>
2020-01-20 20:22:57 +0100

    almost working

--- latticejson/parser.py
+++ latticejson/parser.py
@@ -1,47 +1,50 @@
-from lark import Lark, Transformer
+import os
+from lark import Lark, Transformer, v_args
+from typing import List, Tuple, Dict
+
+DIR_NAME = os.path.dirname(__file__)
+ELEGANT_GRAMMAR_PATH = os.path.join(DIR_NAME, "elegant.lark")
+
+with open(ELEGANT_GRAMMAR_PATH) as file:
+    ELEGANT_GRAMMAR = file.read()
 
 
 class ElegantTransformer(Transformer):
-    def __init__(self):
-        self.elements = {}
-        self.lattices = {}
+    file = lambda self, objects: objects
+    name = lambda self, items: items[0].value
+    type = lambda self, items: items[0].value
+    attribute = tuple
+    int = v_args(inline=True)(int)
+    float = v_args(inline=True)(float)
 
-    def file(self, objects):
-        return objects
-
-    def name(self, s):
-        (s,) = s
-        return s[:]
-
-    def type_(self, s):
-        (s,) = s
-        return s[:]
-
-    def number(self, n):
-        (n,) = n
-        return float(n)
-
-    def element(self, items):
-        name, type_, attributes = items
+    def element(self, items) -> Dict:
+        name, type_, *attributes = items
+        attributes = dict(attributes)
         return {"name": name, "type": type_, **attributes}
 
-    def attributes(self, items):
-        return {key: value for key, value in items}
+    def lattice(self, items) -> Dict:
+        name, arangement = items
+        return {"name": name, "type": "line", "line": arangement}
 
-    def attribute(self, items):
-        key, value = items
-        return key, value
+    def arrangement(self, items) -> List:
+        arrangement = []
+        for item in items:
+            if isinstance(item, str):
+                arrangement.append(item)
+            elif isinstance(item, list):
+                arrangement.extend(item)
+            else:
+                raise Exception(f"Unable to parse arrangement {items}!")
+        return arrangement
 
-    def lattice(self, items):
-        name, lattice = items
-        return {"name": name, "lattice": lattice}
-
-    def names(self, items):
+    def reverse_object(self, items) -> List:
+        items.reverse()
         return items
 
+    @v_args(inline=True)
+    def multiply_object(self, number, object) -> List:
+        return number * object
 
-with open("elegant.lark") as file:
-    elegant_grammar = file.read()
 
-elegant_parser = Lark(elegant_grammar, parser="lalr", start="file")
+elegant_parser = Lark(ELEGANT_GRAMMAR, parser="lalr", start="file")
 elegant_transformer = ElegantTransformer()
---
+++ tests/test_parser.py
@@ -0,0 +1,25 @@
+import os
+from pprint import pprint
+from latticejson.parser import elegant_parser, elegant_transformer
+
+
+path = (
+    "/home/felix/Git/hzb/lattice-development/lattices_B2"
+    "BII_2016-06-10_user_Sym_noID_DesignLattice1996.lte"
+)
+
+base = os.path.dirname(__file__)
+path = os.path.join(base, "data", "fodo_simple.lte")
+path = "/home/felix/Git/elegant/examples/acceptance/fodoScan1/lattice.lte"
+
+
+def test_elegant_parser():
+    with open(path) as file:
+        string = file.read()
+
+    tree = elegant_parser.parse(string)
+    res = elegant_transformer.transform(tree)
+
+    print("\n")
+    # print(tree.pretty())
+    pprint(res)
