8d5b01d7b3f7b1ee83a2894b550a58a61807bd45
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-02 05:15:54 +0100

    WIP

--- latticejson/parser.py
+++ latticejson/parser.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import operator as op
 import math
 from lark import Lark, Transformer, v_args
+from lark.exceptions import VisitError
 from typing import List, Tuple, Dict
 
 DIR_NAME = Path(__file__).resolve().parent
@@ -15,22 +16,31 @@ with open(ELEGANT_GRAMMAR_PATH) as file:
 with open(RPN_GRAMMAR_PATH) as file:
     RPN_GRAMMAR = file.read()
 
-ELEGANT_PARSER = Lark(ELEGANT_GRAMMAR, start="file")
-RPN_PARSER = Lark(RPN_GRAMMAR, parser="lalr", start="rpn_expr")
+ELEGANT_PARSER = Lark(ELEGANT_GRAMMAR, parser="lalr", start="file")
+RPN_PARSER = Lark(RPN_GRAMMAR, parser="lalr", start="start")
 RPN_OPERATORS = {"+": op.add, "-": op.sub, "*": op.mul, "/": op.truediv, "%": op.mod}
+
+
+class UndefinedRPNVariableError(Exception):
+    """Raised if a rpn variable is not defined."""
+
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(f"RPN variable {name} is not defined!", *args, **kwargs)
 
 
 @v_args(inline=True)
 class RPNTransformer(Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.variables = {"pi": math.pi, "e": math.e}
-
     int = int
     float = float
+    word = str
 
-    def rpn(self, result, name) -> Tuple:
-        return name, result
+    def transform(self, tree, variables):
+        self.variables = variables
+        return super().transform(tree)
+
+    def rpn_store(self, result, name) -> Tuple:
+        self.variables[name] = result
+        return result
 
     def rpn_unary(self, operand, operator):
         return getattr(math, operator)(operand)
@@ -43,48 +53,65 @@ class RPNTransformer(Transformer):
         try:
             return self.variables[name]
         except KeyError:
-            raise Exception(f"Variable {name} is not defined!")
+            raise UndefinedRPNVariableError(name)
 
 
 class RPNCalculator:
     def __init__(self):
-        self.transformer = RPNTransformer()
-        self.varibales = self.transformer.variables
+        self.rpn_transformer = RPNTransformer()
+        self.variables = {"pi": math.pi, "e": math.e}
 
-    def calculate(self, string):
-        return self.transformer.transform(RPN_PARSER.parse(string))
-
-    def assign(self, name, value):
-        self.varibales[name] = value
+    def execute(self, string):
+        tree = RPN_PARSER.parse(string)
+        return self.rpn_transformer.transform(tree, self.variables)
 
 
 @v_args(inline=True)
 class ElegantTransformer(Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._variables = {"pi": math.pi, "e": math.e}
-
     int = int
     float = float
-    name = word = rpn_store = str
+    name = word = str
     string = lambda self, item: item[1:-1]
     file = lambda self, *objects: objects
-    attribute = lambda self, name, value: (name, value)
     ref_name = lambda self, item: [item.value]
+    # TODO: better inverse object/arangement
     ref_name_inv = lambda self, item: [item[0] + "_INV"]
-    command = lambda self, *items: items
 
-    def element(self, name, type_, *attributes) -> Dict:
-        return {"name": name, "type": type_, **dict(attributes)}
+    def transform(self, tree):
+        self.rpn_calculator = RPNCalculator()
+        self.elements = {}
+        self.lattices = {}
+        self.commands = []
+        super().transform(tree)
+        return dict(
+            elements=self.elements, lattices=self.lattices, commands=self.commands
+        )
 
-    def lattice(self, name, arangement) -> Dict:
-        return {"name": name, "type": "line", "line": arangement}
+    def element(self, name, type_, *attributes):
+        self.elements[name] = {"type": type_, **dict(attributes)}
+
+    def attribute(self, name, value):
+        if isinstance(value, str):
+            try:
+                value = self.rpn_calculator.execute(value)
+            except VisitError:
+                pass
+        return name, value
+
+    def lattice(self, name, arangement):
+        self.lattices[name] = {"type": "line", "line": arangement}
 
     def arrangement(self, *items) -> List:
         return [sub_item for item in items for sub_item in item]
 
     def mul_object(self, number, object) -> List:
         return number * object
+
+    def rpn_store(self, rpn_string):
+        return self.rpn_calculator.execute(rpn_string)
+
+    def command(self, items):
+        self.commands.append(items)
 
 
 def parse_elegant(string: str):
