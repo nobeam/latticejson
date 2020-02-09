4b14bf5b60c1d38ca47c76073ba0c33e6e20ffcf
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-09 03:36:02 +0100

    Better autoformat (#26)
    * autoformat multiple files at once
    * add newline at end of file

--- latticejson/format.py
+++ latticejson/format.py
@@ -2,17 +2,17 @@ import json
 
 
 class CompactJSONEncoder(json.JSONEncoder):
-    """A JSON Encoder which only indents the first two level."""
+    """A JSON Encoder which only indents the first two levels."""
 
     def encode(self, obj, level=0):
-        if isinstance(obj, (list, tuple)):
-            return f"[{', '.join(json.dumps(el) for el in obj)}]"
-        elif isinstance(obj, dict) and level < 2:
-            indent = (level + 1) * self.indent * " "
-            items = ",\n".join(
-                f"{indent}{json.dumps(key)}: {self.encode(value, level=level+1)}"
+        if isinstance(obj, dict) and level < 2:
+            items_indent = (level + 1) * self.indent * " "
+            items_string = ",\n".join(
+                f"{items_indent}{json.dumps(key)}: {self.encode(value, level=level+1)}"
                 for key, value in obj.items()
             )
-            return f"{{\n{items}\n{level * self.indent * ' '}}}"
+            dict_indent = level * self.indent * " "
+            newline = "\n" if level == 0 else ""
+            return f"{{\n{items_string}\n{dict_indent}}}{newline}"
         else:
             return json.dumps(obj)
