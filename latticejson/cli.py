b00b1c7d8f7f9d4fbb19b1a4566c57e411c0a81c
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-09 02:31:39 +0100

    add initial auto-formatter (#25)
    * add initial auto-formatter
    * some refactoring in cli.py

--- latticejson/cli.py
+++ latticejson/cli.py
@@ -3,9 +3,14 @@ import json
 from pathlib import Path
 
 from .validate import validate_file
-from .io import convert_file
+from .io import convert as _convert
 from .parse import parse_elegant as _parse_elegant
+from .format import CompactJSONEncoder
 from .migrate import migrate as _migrate
+
+
+FORMATS = "json", "lte"
+print_latticejson = lambda obj: print(json.dumps(obj, cls=CompactJSONEncoder, indent=4))
 
 
 @click.group()
@@ -17,14 +22,24 @@ def main():
 @main.command()
 @click.argument("file", type=click.Path(exists=True))
 @click.option(
+    "--from",
+    "from_",
+    type=click.Choice(FORMATS, case_sensitive=False),
+    help="Source format [optional, default: use file extension]",
+)
+@click.option(
     "--to",
     required=True,
-    type=click.Choice(["json", "lte"], case_sensitive=False),
-    help="Destination format.",
+    type=click.Choice(FORMATS, case_sensitive=False),
+    help="Destination format",
 )
-def convert(file, to):
+def convert(file, from_, to):
     """Convert a LatticeJSON or elegant file into another format."""
-    print(convert_file(file, to))
+    path = Path(file)
+    if from_ is None:
+        from_ = path.suffix[1:]
+
+    print(_convert(path.read_text(), from_, to))
 
 
 @main.command()
@@ -39,18 +54,25 @@ def validate(file):
 def parse_elegant(file):
     """Parse elegant file but do not convert to LatticeJSON."""
     text = Path(file).read_text()
-    print(json.dumps(_parse_elegant(text), indent=4))
+    print(json.dumps(_parse_elegant(text), cls=CompactJSONEncoder, indent=4))
 
 
 @main.command()
 @click.argument("file", type=click.Path(exists=True))
-@click.option("--from", "from_", required=True)
-@click.option("--to", required=True)
+def autoformat(file):
+    """Format a LatticeJSON file."""
+    latticejson = json.loads(Path(file).read_text())
+    print(json.dumps(latticejson, cls=CompactJSONEncoder, indent=4))
+
+
+@main.command()
+@click.argument("file", type=click.Path(exists=True))
+@click.option("--from", "from_", required=True, help="Initial version")
+@click.option("--to", required=True, help="Final version")
 def migrate(file, from_, to):
-    """Migrate old LatticeJSON formats to newer versions."""
+    """Migrate old LatticeJSON files to newer versions."""
     text = Path(file).read_text()
     initial_version = from_.split(".")
     final_version = to.split(".")
-    print(f"Migrating {file} from {initial_version} to {final_version}")
     res = _migrate(json.loads(text), initial_version, final_version)
-    print(json.dumps(res, indent=4))
+    print(json.dumps(res, cls=CompactJSONEncoder, indent=4))
---
+++ latticejson/format.py
@@ -0,0 +1,18 @@
+import json
+
+
+class CompactJSONEncoder(json.JSONEncoder):
+    """A JSON Encoder which only indents the first two level."""
+
+    def encode(self, obj, level=0):
+        if isinstance(obj, (list, tuple)):
+            return f"[{', '.join(json.dumps(el) for el in obj)}]"
+        elif isinstance(obj, dict) and level < 2:
+            indent = (level + 1) * self.indent * " "
+            items = ",\n".join(
+                f"{indent}{json.dumps(key)}: {self.encode(value, level=level+1)}"
+                for key, value in obj.items()
+            )
+            return f"{{\n{items}\n{level * self.indent * ' '}}}"
+        else:
+            return json.dumps(obj)
