1aa4189306efa3c74ba15a13379edadb3bed610e
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-14 16:20:49 +0100

    wip

--- latticejson/__init__.py
+++ latticejson/__init__.py
@@ -1,5 +1,5 @@
 from .io import load, convert
 from .validate import validate
-from .convert import elegant_to_latticejson, latticejson_to_elegant
+from .convert import elegant_to_latticejson, latticejson_to_elegant, sort_lattices
 from .migrate import migrate
 from .exceptions import UndefinedObjectError, UndefinedRPNVariableError
