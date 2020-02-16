a6918096ed89acc5a5bb6327abc86e4416fcfdba
Felix Andreas <felix.andreas95@googlemail.com>
2020-02-16 16:02:55 +0100

    add deploy.sh and package.json

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:nobeam/latticejson.git master:gh-pages
+git push -f git@github.com:nobeam/latticejson.git master:gh-pages
 
 cd -
