811340bdfde16d6c952fa33e4cf28d218233e0df
Felix Andreas <felix.andreas95@googlemail.com>
2020-04-17 10:40:40 +0200

    push all

--- examples/example.js
+++ examples/example.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync("fodo.json"));
 
 const elementName = "Q1";
-const [type, { length }] = data.elements["Q1"];
+const [type, { length }] = data.elements[elementName];
 console.log(`The element ${elementName} is a ${type} and is ${length} meters long.`);
