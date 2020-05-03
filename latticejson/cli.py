2e93876b45e3565442525830892b9d6ec0216626
Felix Andreas <felix.andreas95@googlemail.com>
2020-05-04 00:02:00 +0200

    wip

--- latticejson/cli.py
+++ latticejson/cli.py
@@ -6,8 +6,8 @@ import click
 
 from . import __version__, io, parse
 from .format import format_json
-from .migrate import migrate as _migrate
-from .validate import schema, validate_file
+from .migrate import migrate as _migrate, MAX_VERSION
+from .validate import schema, validate_file, parse_version
 
 FORMATS = "json", "lte", "madx"
 
@@ -73,14 +73,30 @@ def autoformat(files, dry_run):
 
 
 @cli.command()
-@click.argument("file", type=click.Path(exists=True))
-@click.option("--initial", type=(int, int, int), required=True, help="Initial version")
-@click.option("--final", type=(int, int, int), help="Final version (default: latest)")
-def migrate(file, initial, final):
+@click.argument("files", nargs=-1, type=click.Path(exists=True))
+@click.option(
+    "--final", type=int, default=MAX_VERSION, show_default=True, help="Final version."
+)
+@click.option(
+    "--dry-run",
+    "-d",
+    is_flag=True,
+    help="Don't write the files back, just output the formatted files.",
+)
+def migrate(files, final, dry_run):
     """Migrate old LatticeJSON files to newer versions."""
-    text = Path(file).read_text()
-    latticejson = _migrate(json.loads(text), initial.split("."), final.split("."))
-    click.echo(format_json(latticejson))
+    for path in itertools.chain.from_iterable(
+        path.rglob("*.json") if path.is_dir() else (path,) for path in map(Path, files)
+    ):
+        data = json.loads(path.read_text())
+        initial = parse_version(data["version"]).major
+        latticejson = _migrate(data, initial, final)
+        formatted = format_json(latticejson)
+        click.secho(f"Migrated {path} from version {initial} to {final}", bold=True)
+        if dry_run:
+            click.echo(formatted)
+        else:
+            path.write_text(formatted)
 
 
 @cli.group()
--- latticejson/migrate.py
+++ latticejson/migrate.py
@@ -1,18 +1,12 @@
-from typing import Optional, Tuple
-
-Version = Tuple[int, int, int]
-
-
-def migrate(data: dict, initial: Version, final: Optional[Version] = None) -> dict:
+def migrate(data: dict, initial: int, final: int) -> dict:
     data = data.copy()
     for from_, to, func in _VERSION_MAPS:
-        if initial >= from_ and (final is None or final <= to):
+        if from_ >= initial and (final is None or to <= final):
             func(data)
     return data
 
 
 def _0_to_1(data: dict):
-    data["version"] = "1.0"
     elements = data["elements"]
     for name, attributes in elements.items():
         elements[name] = attributes.pop("type"), attributes
@@ -21,14 +15,18 @@ def _0_to_1(data: dict):
 def _1_to_2(data: dict):
     data["version"] = "2.0"
     data["title"] = data.pop("name")
-    data["info"] = data.pop("description")
     data["lattices"] = data.pop("sub_lattices")
     data["lattices"]["__MAIN__"] = data.pop("lattice")
     data["root"] = "__MAIN__"
-    for element in data["elements"]:
-        info = element.pop("description", False)
+    info = data.pop("description", False)
+    if info:
+        data["info"] = info
+
+    for _, attributes in data["elements"].values():
+        info = attributes.pop("description", False)
         if info:
-            element["info"] = info
+            attributes["info"] = info
 
 
 _VERSION_MAPS = (0, 1, _0_to_1), (1, 2, _1_to_2)
+MAX_VERSION = _VERSION_MAPS[-1][1]
