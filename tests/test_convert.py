929b570f69defa491ffdc70c996fa0f27fec941b
Felix Andreas <felix.andreas95@googlemail.com>
2020-05-26 10:30:27 +0000

    Automated Code Formatting using Black

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -10,26 +10,29 @@ fodo_json = json.loads((base_dir / "fodo.json").read_text())
 
 
 def test_to_elegant():
-    expected = ('! TITLE: FODO Lattice\n'
- 'M1: MARK, \n'
- 'D1: DRIF, L=0.55\n'
- 'Q1: KQUAD, L=0.2, K1=1.2\n'
- 'Q2: KQUAD, L=0.4, K1=-1.2\n'
- 'B1: CSBEND, L=1.5, ANGLE=0.39269908169872414, E1=0.19634954084936207, '
- 'E2=0.19634954084936207\n'
- 'CELL: LINE=(M1, Q1, D1, B1, D1, Q2, D1, B1, D1, Q1)\n'
- 'RING: LINE=(CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL)\n'
- 'USE, RING\n')
+    expected = (
+        "! TITLE: FODO Lattice\n"
+        "M1: MARK, \n"
+        "D1: DRIF, L=0.55\n"
+        "Q1: KQUAD, L=0.2, K1=1.2\n"
+        "Q2: KQUAD, L=0.4, K1=-1.2\n"
+        "B1: CSBEND, L=1.5, ANGLE=0.39269908169872414, E1=0.19634954084936207, "
+        "E2=0.19634954084936207\n"
+        "CELL: LINE=(M1, Q1, D1, B1, D1, Q2, D1, B1, D1, Q1)\n"
+        "RING: LINE=(CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL)\n"
+        "USE, RING\n"
+    )
 
     elegant = to_elegant(fodo_json)
     print(elegant)
     assert expected == elegant
 
+
 def test_from_elegant():
     latticejson = from_elegant(fodo_lte)
-    latticejson.pop('title')
-    fodo_json.pop('info')
-    fodo_json.pop('title')
+    latticejson.pop("title")
+    fodo_json.pop("info")
+    fodo_json.pop("title")
     print(latticejson)
     assert sorted(latticejson.items()) == sorted(fodo_json.items())
 
