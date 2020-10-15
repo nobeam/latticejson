6bb3a382ed15412fcdd22880bcd2fdf3551a9e44
Felix Andreas <felix.andreas95@googlemail.com>
2020-10-15 12:04:45 +0200

    fix bug: add keyword sequence when converting to madx (#66)

--- latticejson/convert.py
+++ latticejson/convert.py
@@ -117,5 +117,5 @@ def to_madx(latticejson: dict) -> str:
     for name, children in sort_lattices(latticejson).items():
         strings.append(lattice_template(name, ", ".join(children)))
 
-    strings.append(f"USE, {latticejson['root']};\n")
+    strings.append(f"USE, SEQUENCE={latticejson['root']};\n")
     return "\n".join(strings)
